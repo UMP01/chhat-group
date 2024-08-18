8a534f5382f8c5339fd410644416e5981a1db8b7
hutleangchhun <hutleangchhun@gmail.com>
2024-08-18 15:32:47 +0700

    Update responsive in About us page

--- frontend/src/Pages/Website/AboutUs.jsx
+++ frontend/src/Pages/Website/AboutUs.jsx
@@ -17,80 +17,77 @@ const About = () => {
     const Values = [
         "To offer world-class and high standard products and services",
         "To be a globally recognized and trusted business partner",
-        "To ensure a transparency and accountability",
+        "To ensure transparency and accountability",
         "To work collaboratively with customers and trade partners",
         "To ensure environmental friendliness",
         "To empower employees and ensure equality for all",
     ];
-
     return (
         <>
-            <Breadcrumb curPage="About US" />
-            <div className="container max-w-7xl h-[550px] mx-auto px-5 pb-5 pt-8 rubik bg-[#f8f8f8] rounded flex">
-                <div className="w-[500px] h-[500px]  rounded">
-                    <div className="w-full h-[300px]">
-                        <img
-                            src={Banner}
-                            alt=""
-                            className="w-full h-full rounded"
-                        />
+            <div className="bg-[#f8f8f8]">
+                <Breadcrumb curPage="About US" />
+                <div className="container max-w-7xl mx-auto px-5 pb-5 pt-8 rubik rounded flex flex-col lg:flex-row gap-5">
+                    <div className="lg:w-1/2 flex flex-col gap-4">
+                        <div className="w-full rounded">
+                            <img
+                                src={Banner}
+                                alt="Banner"
+                                className="w-full h-auto rounded"
+                            />
+                        </div>
+                        <div className="w-full">
+                            <h1 className="text-sm text-[#0191DA] uppercase font-bold">
+                                Core Values
+                            </h1>
+                            <div className="w-full h-[1px] bg-[#D9D9D9] mt-1"></div>
+                            <ul className="text-sm pt-2 pl-5">
+                                {Values.map((title, index) => (
+                                    <li className="list-disc" key={index}>
+                                        {title}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
                     </div>
-                    <div className="w-full h-[100px] pt-4">
-                        <h1 className="text-sm text-[#0191DA]  uppercase font-bold">
-                            Core Values
-                        </h1>
-                        <div className="w-[460px] h-[1px] bg-[#D9D9D9]  mt-1"></div>
-                        <ul className="text-sm pt-2 pl-5">
-                            {Values.map((title, index) => (
-                                <li className="list-disc" key={index}>
-                                    {title}
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                </div>
-                <div className="w-[740px] h-[50px] ml-3">
-                    <div className="w-full h-[120px]  ">
-                        <h1 className="text-sm text-[#0191DA] pl-3 uppercase font-bold">
-                            Vision
-                        </h1>
-                        <div className="w-[730px] h-[1px] bg-[#D9D9D9] mx-3 mt-1"></div>
-                        <p className="text-sm pt-2 pl-5">
-                            To be the most trusted Trading, Import and Export
-                            Company, widely recognized by our customers and
-                            trade partners in Cambodia and overseas. We will do
-                            this by providing world-class quality products and
-                            services to our customers and trade part ners
-                            through our newly innovated e-commerce platform for
-                            an easy and convenient shopping experience.
-                        </p>
-                    </div>
-                    <div className="w-full h-[230px]">
-                        <h1 className="text-sm text-[#0191DA] pl-3 uppercase font-bold">
-                            Mission
-                        </h1>
-                        <div className="w-[730px] h-[1px] bg-[#D9D9D9] mx-3 mt-1"></div>
-                        <ul className="text-sm pt-2 pl-8">
-                            {Mission.map((title, index) => (
-                                <li className="list-decimal" key={index}>
-                                    {title}
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                    {/* GOAL */}
-                    <div className="w-full h-[100px] ">
-                        <h1 className="text-sm text-[#0191DA] pl-3 uppercase font-bold">
-                            Goal
-                        </h1>
-                        <div className="w-[730px] h-[1px] bg-[#D9D9D9] mx-3 mt-1"></div>
-                        <ul className="text-sm pt-2 pl-8">
-                            {Goal.map((title, index) => (
-                                <li className="list-disc" key={index}>
-                                    {title}
-                                </li>
-                            ))}
-                        </ul>
+                    <div className="lg:w-1/2 flex flex-col gap-4">
+                        <div className="w-full">
+                            <h1 className="text-sm text-[#0191DA] pl-3 uppercase font-bold">
+                                Vision
+                            </h1>
+                            <div className="w-full h-[1px] bg-[#D9D9D9] mx-3 mt-1"></div>
+                            <p className="text-sm pt-2 pl-5">
+                                To be the most trusted Trading, Import and
+                                Export Company, widely recognized by our
+                                customers and trade partners in Cambodia and
+                                overseas. We will do this by providing
+                                world-class quality products and services to our
+                                customers and trade partners through our newly
+                                innovated e-commerce platform for an easy and
+                                convenient shopping experience.
+                            </p>
+                        </div>
+                        <div className="w-full">
+                            <h1 className="text-sm text-[#0191DA] pl-3 uppercase font-bold">
+                                Mission
+                            </h1>
+                            <div className="w-full h-[1px] bg-[#D9D9D9] mx-3 mt-1"></div>
+                            <ul className="text-sm pt-2 pl-8 list-decimal">
+                                {Mission.map((title, index) => (
+                                    <li key={index}>{title}</li>
+                                ))}
+                            </ul>
+                        </div>
+                        <div className="w-full">
+                            <h1 className="text-sm text-[#0191DA] pl-3 uppercase font-bold">
+                                Goal
+                            </h1>
+                            <div className="w-full h-[1px] bg-[#D9D9D9] mx-3 mt-1"></div>
+                            <ul className="text-sm pt-2 pl-8 list-disc">
+                                {Goal.map((title, index) => (
+                                    <li key={index}>{title}</li>
+                                ))}
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
