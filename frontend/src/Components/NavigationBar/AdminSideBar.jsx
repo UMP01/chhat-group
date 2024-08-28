0678c508831a1aacafcc453c024a2bd04a2593d8
phoudyum <phoudyum@gmail.com>
2024-08-29 00:15:25 +0700

    remove bg navbar

--- frontend/src/Components/NavigationBar/AdminSideBar.jsx
+++ frontend/src/Components/NavigationBar/AdminSideBar.jsx
@@ -15,31 +15,31 @@ const AdminSideBar = () => {
                 <h1 className="text-2xl font-bold text-gray-300">Chhat Group</h1>
             </div>
             <ul className="space-y-4">
-                <li className={`flex items-center cursor-pointer transition duration-200 ease-in-out transform ${active === 0 ? 'bg-primary text-white font-bold' : 'text-gray-300 hover:text-primary'}`} onClick={() => handleClick(0)}>
+                <li className={`flex items-center cursor-pointer transition duration-200 ease-in-out transform ${active === 0 ? 'text-white font-bold' : 'text-gray-300 hover:text-primary'}`} onClick={() => handleClick(0)}>
                     <Link to="/admin/dashboard" className="flex items-center w-full">
                         <HomeIcon className="w-6 h-6 mr-2" />
                         <span>Dashboard</span>
                     </Link>
                 </li>
-                <li className={`flex items-center cursor-pointer transition duration-200 ease-in-out transform ${active === 1 ? 'bg-primary text-white font-bold' : 'text-gray-300 hover:text-primary'}`} onClick={() => handleClick(1)}>
+                <li className={`flex items-center cursor-pointer transition duration-200 ease-in-out transform ${active === 1 ? 'text-white font-bold' : 'text-gray-300 hover:text-primary'}`} onClick={() => handleClick(1)}>
                     <Link to="/admin/chhat-group-blog" className="flex items-center w-full">
                         <NewspaperIcon className="w-6 h-6 mr-2" />
                         <span>Chhat Group Blog</span>
                     </Link>
                 </li>
-                <li className={`flex items-center cursor-pointer transition duration-200 ease-in-out transform ${active === 2 ? 'bg-primary text-white font-bold' : 'text-gray-300 hover:text-primary'}`} onClick={() => handleClick(2)}>
+                <li className={`flex items-center cursor-pointer transition duration-200 ease-in-out transform ${active === 2 ? 'text-white font-bold' : 'text-gray-300 hover:text-primary'}`} onClick={() => handleClick(2)}>
                     <Link to="/admin/chhat-research-blog" className="flex items-center w-full">
                         <NewspaperIcon className="w-6 h-6 mr-2" />
                         <span>Chhat Research Blog</span>
                     </Link>
                 </li>
-                <li className={`flex items-center cursor-pointer transition duration-200 ease-in-out transform ${active === 3 ? 'bg-primary text-white font-bold' : 'text-gray-300 hover:text-primary'}`} onClick={() => handleClick(3)}>
+                <li className={`flex items-center cursor-pointer transition duration-200 ease-in-out transform ${active === 3 ? 'text-white font-bold' : 'text-gray-300 hover:text-primary'}`} onClick={() => handleClick(3)}>
                     <Link to="/admin/career" className="flex items-center w-full">
                         <BriefcaseIcon className="w-6 h-6 mr-2" />
                         <span>Career</span>
                     </Link>
                 </li>
-                <li className={`flex items-center cursor-pointer transition duration-200 ease-in-out transform ${active === 4 ? 'bg-primary text-white font-bold' : 'text-gray-300 hover:text-primary'}`} onClick={() => handleClick(4)}>
+                <li className={`flex items-center cursor-pointer transition duration-200 ease-in-out transform ${active === 4 ? 'text-white font-bold' : 'text-gray-300 hover:text-primary'}`} onClick={() => handleClick(4)}>
                     <Link to="/admin/user" className="flex items-center w-full">
                         <UserGroupIcon className="w-6 h-6 mr-2" />
                         <span>User</span>
