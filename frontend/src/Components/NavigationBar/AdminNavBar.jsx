dae05eca8bd86bbd395a0145673e81b32f76ab60
hutleangchhun <hutleangchhun@gmail.com>
2024-10-12 22:55:57 +0700

    Update Admin Navbar

--- frontend/src/Components/NavigationBar/AdminNavBar.jsx
+++ frontend/src/Components/NavigationBar/AdminNavBar.jsx
@@ -3,15 +3,15 @@ import {
     ArrowLeftEndOnRectangleIcon,
     BellIcon,
     UserIcon,
-} from "@heroicons/react/24/solid"; // Updated import
+    XCircleIcon,
+    Bars3Icon,
+} from "@heroicons/react/24/solid";
 import UserAvatar from "../../assets/Images/logo.png"; // Update path if needed
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom"; // Import Link for routing
 
-const AdminNavBar = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const location = useLocation();
-
-    // Get user name from local storage
+const AdminNavBar = ({ toggleSidebar, isOpen }) => {
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false); // State to manage dropdown visibility
+    const location = useLocation(); // Get the current location
     const userName = localStorage.getItem("userName") || "User"; // Default to "User"
 
     const getTitle = () => {
@@ -31,62 +31,90 @@ const AdminNavBar = () => {
             case "/admin/profile":
                 return "Profile";
             default:
-                return "Admin Panel";
+                return "Admin Panel"; // Default title
         }
     };
 
-    const title = getTitle();
+    const title = getTitle(); // Get the title based on URL
+
+    const handleSidebarToggle = () => {
+        toggleSidebar();
+        // Close the dropdown when toggling the sidebar
+        if (isDropdownOpen) {
+            setIsDropdownOpen(false);
+        }
+    };
+
+    const handleDropdownToggle = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
 
     return (
-        <nav className="bg-white dark:bg-boxdark shadow py-4 px-6">
-            <div className="flex items-center justify-between">
-                <div className="text-xl ml-12 font-normal text-gray-800">
-                    {title}
+        <nav className="bg-white dark:bg-boxdark shadow py-4 px-6 flex justify-between items-center">
+            <button
+                className="text-gray-300 focus:outline-none"
+                onClick={handleSidebarToggle} // Use the new function here
+            >
+                <span className="sr-only">
+                    {isOpen ? "Close Sidebar" : "Open Sidebar"}
+                </span>
+                {isOpen ? (
+                    <XCircleIcon className="w-10 h-10" />
+                ) : (
+                    <Bars3Icon className="w-10 h-10 text-cyan-700" />
+                )}
+            </button>
+            <h1 className="text-xl text-gray-800">{title}</h1>
+            <div className="flex items-center space-x-4">
+                <div className="relative">
+                    <BellIcon className="w-6 h-6 text-gray-600 hover:text-primary" />
+                    <span className="absolute top-0 right-0 flex items-center justify-center w-3 h-3 bg-red-600 text-white text-xs rounded-full">
+                        3
+                    </span>
                 </div>
-                <div className="flex items-center space-x-4">
-                    <div className="relative">
-                        <BellIcon className="w-6 h-6 text-gray-600 hover:text-primary" />
-                        <span className="absolute top-0 right-0 flex items-center justify-center w-3 h-3 bg-red-600 text-white text-xs rounded-full">
-                            3
+                <div className="relative">
+                    <button
+                        className="flex items-center space-x-2 focus:outline-none"
+                        onClick={handleDropdownToggle} // Toggle dropdown
+                    >
+                        <img
+                            className="w-8 h-8 rounded-full border-2 border-gray-300"
+                            src={UserAvatar}
+                            alt="User Avatar"
+                        />
+                        <span className="font-normal text-gray-800">
+                            {userName} {/* Display the user name */}
                         </span>
-                    </div>
-                    <div className="relative">
-                        <button
-                            className="flex items-center space-x-2 focus:outline-none"
-                            onClick={() => setIsOpen(!isOpen)}
-                        >
-                            <img
-                                className="w-8 h-8 rounded-full border-2 border-gray-300"
-                                src={UserAvatar}
-                                alt="User Avatar"
-                            />
-                            <span className="font-normal text-gray-800">
-                                {userName} {/* Display the user name */}
-                            </span>
-                        </button>
-                        {isOpen && (
-                            <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-10 border">
-                                <ul className="py-2">
-                                    <li>
-                                        <a
-                                            href="/admin/profile"
-                                            className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                                        >
-                                            Profile
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a
-                                            href="/admin/login"
-                                            className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                                        >
-                                            Logout
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        )}
-                    </div>
+                    </button>
+
+                    {isDropdownOpen && (
+                        <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-10 border">
+                            <ul className="py-2">
+                                <li>
+                                    <Link
+                                        to="/admin/profile"
+                                        className="block px-4 py-2 text-gray-700 hover:bg-gray-100 flex items-center"
+                                    >
+                                        <UserIcon className="w-5 h-5 mr-2" />
+                                        Profile
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        to="/admin/login"
+                                        className="block px-4 py-2 text-gray-700 hover:bg-gray-100 flex items-center"
+                                        onClick={() => {
+                                            setIsDropdownOpen(false);
+                                            // You can add logout logic here if needed
+                                        }}
+                                    >
+                                        <ArrowLeftEndOnRectangleIcon className="w-5 h-5 mr-2" />
+                                        Logout
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    )}
                 </div>
             </div>
         </nav>
