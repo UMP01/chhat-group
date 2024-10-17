4016df77a6750b812325fe2895d46c243a2b8700
hutleangchhun <hutleangchhun@gmail.com>
2024-10-17 21:30:40 +0700

    Update UI Blog Detail

--- frontend/src/Components/ContentChhatReasech/BlogDetail.jsx
+++ frontend/src/Components/ContentChhatReasech/BlogDetail.jsx
@@ -45,33 +45,41 @@ const BlogDetail = () => {
             {article && (
                 <>
                     <div className="py-5 px-5 border-2 rounded-md">
-                        <h1 className="text-md sm:text-md md:text-lg lg:text-xl font-semibold mb-4 text-gray-700">
+                        <h1 className="text-md sm:text-lg md:text-xl lg:text-2xl font-semibold mb-4 text-gray-700">
                             {article.title}
                         </h1>
                         <div className="flex items-center text-gray-700">
                             <BsCalendarDate className="mr-2" />
                             <p className="text-sm font-medium">
-                                Posted on:
+                                Posted on:{" "}
                                 {new Date(
                                     article.created_at
                                 ).toLocaleDateString()}
                             </p>
                         </div>
                     </div>
-                    <div className="flex items-start justify-center font-medium">
-                        <div className="lg:w-1/2 px-10 py-5 ">
+                    <div className="flex flex-col md:flex-row items-start justify-between font-medium">
+                        <div className="w-full md:w-1/2 px-5 py-5">
+                            {article.image && (
+                                <img
+                                    src={`http://127.0.0.1:8000/storage/${article.image}`}
+                                    alt={article.title || "News image"}
+                                    className="w-full object-cover rounded"
+                                />
+                            )}
+                        </div>
+                        <div className="px-5 py-5 w-full md:w-1/2">
                             <p className="text-gray-700 text-sm sm:text-base md:text-base lg:text-base">
                                 {article.content}
                             </p>
-                            <div className="text-gray-700">
+                            <div className="text-gray-700 mt-4">
                                 <p>ព័ត៌មានបន្ថែម៖</p>
                                 <ul>
                                     <li>012 890 801</li>
                                     <li>010 969 005</li>
-                                    <li></li>
                                 </ul>
                             </div>
-                            <div className="text-gray-700">
+                            <div className="text-gray-700 mt-4">
                                 <p>អាស័យដ្ឋាន៖</p>
                                 <p>
                                     បុរីពិភពថ្មី ឡាសែនសុខពីរ ផ្ទះលេខ 43-45 ផ្លូវ
@@ -80,50 +88,39 @@ const BlogDetail = () => {
                                     រាជធានីភ្នំពេញ។
                                 </p>
                             </div>
-                            <div className="text-gray-700">
+                            <div className="text-gray-700 mt-4">
                                 <p>Contact Us</p>
-                                <div className="flex gap-2">
+                                <div className="flex flex-wrap gap-2 mt-2">
                                     <a
-                                        href="https://l.facebook.com/l.php?u=https%3A%2F%2Ft.me%2Fjoinchat%2FJZW3WDMdHgkzODk1%3Ffbclid%3DIwZXh0bgNhZW0CMTAAAR2Ne6tpdWUrPK4y6yBECUz4zawnf0I-1VNGULEXfYTyl_4uQNWbO5MzBtI_aem_AVXafXlRmLb7pQbsLyyrwMG--jK-YIUbmazjVcegeONxVvkw2FDI-Hzf-jbTc4q2-HXIH1gBNew9URZo65NL_4bW&h=AT0idutihjCtld65gS92rUPdhJb7aHOx80Y5qzEuTniYdcwD8cqxmkc-TphyTDrxTFExG_MZJcQGdzvMsQQP-0XyUwLgYfEYGk4_MaC_GNB2BMrHdAP2uDOLTRIr53OP7LQ2&__tn__=-UK-R&c[0]=AT1fj79G4GXRgBucUTAPnu_8NzziVLW3mDg2W4JOE5kH-0wGyS_J5JvQ9vX1uBFB65U0HT5LBCmseGTte2TAAWWmNQYsYLPV_NyZ5CQNOBef0vfsQwSNAtBF_HY55dYReOgSFPbFHfI7gJROlR1Va1AUQGo76fLRjXhKsqBKiyuPojpXsRXGcIa1EONphmYZZU3cRsyKU_ObshMf9I6C5H2kLfvjcA3H2MSS"
-                                        className=" px-4 py-2 bg-cyan-700 rounded-md text-sm flex items-center text-white hover:bg-white hover:text-cyan-700 duration-500 ease-in-out"
+                                        href="https://t.me/joinchat/JZW3WDMdHgkzODk1"
+                                        className="p-4 bg-cyan-700 rounded-full flex items-center text-white hover:bg-white hover:text-cyan-700 duration-500 ease-in-out"
                                     >
-                                        <FaTelegramPlane className="mr-2" />
-                                        Telegram
+                                        <FaTelegramPlane />
                                     </a>
                                     <a
                                         href="https://facebook.com/chhatgroup"
                                         target="_blank"
-                                        className=" px-4 py-2 bg-cyan-700 rounded-md text-sm flex items-center text-white hover:bg-white hover:text-cyan-700 duration-500 ease-in-out"
+                                        rel="noopener noreferrer"
+                                        className="p-4 bg-cyan-700 rounded-full flex items-center text-white hover:bg-white hover:text-cyan-700 duration-500 ease-in-out"
                                     >
-                                        <FaFacebookF className="mr-2" />
-                                        Facebook
+                                        <FaFacebookF />
                                     </a>
                                     <a
+                                        href="https://instagram.com/chhatgroup"
                                         target="_blank"
-                                        href="https://facebook.com/chhatgroup"
-                                        className=" px-4 py-2 bg-cyan-700 rounded-md text-sm flex items-center text-white hover:bg-white hover:text-cyan-700 duration-500 ease-in-out"
+                                        rel="noopener noreferrer"
+                                        className="p-4 bg-cyan-700 rounded-full flex items-center text-white hover:bg-white hover:text-cyan-700 duration-500 ease-in-out"
                                     >
-                                        <FaInstagram className="mr-2" />
-                                        Instagram
+                                        <FaInstagram />
                                     </a>
                                     <a
                                         href="mailto:info@chhatgroup.com"
-                                        className=" px-4 py-2 bg-cyan-700 rounded-md text-sm flex items-center text-white hover:bg-white hover:text-cyan-700 duration-500 ease-in-out"
+                                        className="p-4 bg-cyan-700 rounded-full flex items-center text-white hover:bg-white hover:text-cyan-700 duration-500 ease-in-out"
                                     >
-                                        <IoMdMail className="mr-2" />
-                                        Mail
+                                        <IoMdMail />
                                     </a>
                                 </div>
                             </div>
-                        </div>
-                        <div className="lg:w-1/2 px-10 py-5">
-                            {article.image && (
-                                <img
-                                    src={`http://127.0.0.1:8000/storage/${article.image}`}
-                                    alt={article.title || "News image"}
-                                    className="w-full object-cover mb-4 rounded"
-                                />
-                            )}
                         </div>
                     </div>
                 </>
