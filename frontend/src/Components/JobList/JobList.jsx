0deea9ff0335845a24ae3fa43ac98a099c1fd89d
hutleangchhun <hutleangchhun@gmail.com>
2024-10-29 13:25:39 +0700

    Update loading spin and error message

--- frontend/src/Components/JobList/JobList.jsx
+++ frontend/src/Components/JobList/JobList.jsx
@@ -4,110 +4,134 @@ import { IoLocationOutline } from "react-icons/io5";
 import { IoTimeOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
 const JobList = () => {
-  const [jobs, setJobs] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+    const [jobs, setJobs] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-  const fetchJobs = async () => {
-    setLoading(true);
-    try {
-      const response = await axiosClient.get("/careers");
+    const fetchJobs = async () => {
+        setLoading(true);
+        try {
+            const response = await axiosClient.get("/careers");
             setJobs(Array.isArray(response.data) ? response.data : []);
-    } catch (error) {
-      console.error("Error fetching jobs:", error);
-      setError("Failed to fetch job listings");
-    }finally{
-      setLoading(false);
+        } catch (error) {
+            console.error("Error fetching jobs:", error);
+            setError("Failed to fetch job listings");
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchJobs();
+    }, []);
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        setJobs(searchTerm);
+    };
+
+    if (loading) {
+        return (
+            <div className="min-h-screen flex items-center justify-center">
+                <div className="flex justify-center items-center border-gray-300 h-7 w-7 animate-spin rounded-full border-2 border-t-sky-700"></div>
+            </div>
+        );
     }
-  };
+    if (error) {
+        return (
+            <div className="max-w-7xl mx-auto text-cyan-700">
+                <div className="flex flex-col text-center items-center py-32">
+                    <div className="w-full">
+                        <h1 className="font-semibold text-5xl sm:text-5xl md:text-6xl lg:text-7xl">
+                            Sorry!
+                        </h1>
+                    </div>
+                    <div className="w-full py-5">
+                        <p className="font-medium text-lg sm:text-lg md:text-lg lg:text-xl">
+                            {error}
+                        </p>
+                    </div>
+                    <a
+                        href="/"
+                        className="py-2 border-b-2 font-medium border-cyan-700 hover:scale-110 duration-200 text-sm sm:text-base md:text-base lg:text-base"
+                    >
+                        Back to homepage
+                    </a>
+                </div>
+            </div>
+        );
+    }
 
-  useEffect(() => {
-    fetchJobs();
-  }, []);
-
-  const handleSearch = (e) => {
-    e.preventDefault();
-    setJobs(searchTerm);
-  };
-
-  if (loading) {
     return (
-      <p>Loading...</p>
+        <div className="rubik">
+            <form
+                onSubmit={handleSearch}
+                className="search-container flex items-center mb-4"
+            >
+                <input
+                    type="text"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    className="w-full py-2 pl-4 pr-20 rounded-l-md text-gray-700 border-t-2 border-l-2 border-b-2 border-sky-600 focus:outline-none focus:ring-0 focus:ring-sky-600"
+                    placeholder="Search for jobs..."
+                />
+                <button
+                    type="submit"
+                    className="py-2 px-4 bg-sky-600 text-white border-2 border-sky-600 rounded-r-md hover:bg-sky-700"
+                >
+                    Search
+                </button>
+            </form>
+            {error && <p className="text-red-500">{error}</p>}
+            <div className="mt-4">
+                <ul className="space-y-2">
+                    {jobs.length > 0 ? (
+                        jobs.map((job) => (
+                            <li
+                                key={job.id}
+                                className="py-2 px-5 rounded-md shadow-md border border-gray-100"
+                            >
+                                <h3 className="text-xl text-gray-700 py-1">
+                                    {job.title}
+                                </h3>
+                                <div className="flex">
+                                    <IoLocationOutline className="mt-3" />
+                                    <h3 className=" text-gray-700 pb-3 pt-2 pl-2">
+                                        {job.location}
+                                    </h3>
+                                </div>
+                                <div className="flex flex-row justify-between text-center">
+                                    <div className="flex">
+                                        <IoTimeOutline className="mt-1" />
+                                        <h3 className=" text-gray-700 pb-3 pl-2">
+                                            DateLine:
+                                            {new Date(
+                                                job.dateline
+                                            ).toLocaleDateString()}
+                                        </h3>
+                                    </div>
+                                    <Link to={`/careerdetails/${job.id}`}>
+                                        <a
+                                            href=""
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="text-cyan-700 text-sm font-medium
+                                    hover:text-cyan-600 duration-75"
+                                        >
+                                            View More
+                                        </a>
+                                    </Link>
+                                </div>
+                            </li>
+                        ))
+                    ) : (
+                        <p>No jobs found</p>
+                    )}
+                </ul>
+            </div>
+        </div>
     );
-  }
-
-  return (
-    <div className="rubik">
-      <form
-        onSubmit={handleSearch}
-        className="search-container flex items-center mb-4"
-      >
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          className="w-full py-2 pl-4 pr-20 rounded-l-md text-gray-700 border-t-2 border-l-2 border-b-2 border-sky-600 focus:outline-none focus:ring-0 focus:ring-sky-600"
-          placeholder="Search for jobs..."
-        />
-        <button
-          type="submit"
-          className="py-2 px-4 bg-sky-600 text-white border-2 border-sky-600 rounded-r-md hover:bg-sky-700"
-        >
-          Search
-        </button>
-      </form>
-      {error && <p className="text-red-500">{error}</p>}
-      <div className="mt-4">
-        <h2 className="text-2xl my-5 text-gray-700 font-semibold">Announcement</h2>
-        <ul className="space-y-2">
-          {jobs.length > 0 ? (
-            jobs.map((job) => (
-              <li
-                key={job.id}
-                className="p-3 bg-gray-50 rounded-md shadow-md border border-gray-100"
-              >
-                <h3 className="text-lg text-gray-700 py-1 font-semibold">{job.title}</h3>
-                <div className="flex">
-                  <IoLocationOutline className="mt-3"/>
-                  <h3 className="text-[15px] text-gray-500 pb-3 pt-2 pl-2">
-                    {job.location}
-                  </h3>
-                </div>
-                <div className="flex flex-row justify-between text-center">
-                  <div className="flex">
-                    <IoTimeOutline className="mt-1"/>
-                    <h3 className="text-sm text-gray-500 pb-3 pl-2">
-                    DateLine: 
-                    {new Date(
-                          job.dateline
-                      ).toLocaleDateString()}
-                    </h3>
-                    
-                  </div>
-                  <Link to={`/careerdetails/${job.id}`}>
-                      <a
-                        href=""
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-sky-400 text-[12px] font-semibold
-                                    hover:text-sky-600 duration-75"
-                      >
-                      View More
-                      </a>
-                    </Link>
-                </div>
-                
-                
-              </li>
-            ))
-          ) : (
-            <p>No jobs found</p>
-          )}
-        </ul>
-      </div>
-    </div>
-  );
 };
 
 export default JobList;
--- frontend/src/Pages/Website/CareerDetails.jsx
+++ frontend/src/Pages/Website/CareerDetails.jsx
@@ -1,111 +1,148 @@
-import React, { useState,useEffect } from 'react'
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { axiosClient } from "../../api/axios";
 
 const CareerDetails = () => {
-  const { id } = useParams();
-  const [jobs, setJobs] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+    const { id } = useParams();
+    const [jobs, setJobs] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-  const fetchJobs = async () => {
-    setLoading(true);
-    try {
-      const response = await axiosClient.get(`/careers/${id}`);
+    const fetchJobs = async () => {
+        setLoading(true);
+        try {
+            const response = await axiosClient.get(`/careers/${id}`);
             setJobs(response.data);
-    } catch (error) {
-      console.error("Error fetching jobs:", error);
-      setError("Failed to fetch job listings");
-    }finally{
-      setLoading(false);
+        } catch (error) {
+            console.error("Error fetching jobs:", error);
+            setError("Failed to fetch job listings");
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchJobs();
+    }, [id]);
+
+    if (loading) {
+        return (
+            <div className="min-h-screen flex items-center justify-center flex-col">
+                <div className="flex justify-center items-center border-gray-300 h-7 w-7 animate-spin rounded-full border-2 border-t-sky-700"></div>
+            </div>
+        );
     }
-  };
 
-  useEffect(() => {
-    fetchJobs();
-  }, [id]);
+    if (error) {
+        return (
+            <div className="max-w-7xl mx-auto text-cyan-700">
+                <div className="flex flex-col text-center items-center py-32">
+                    <div className="w-full">
+                        <h1 className="font-semibold text-5xl sm:text-5xl md:text-6xl lg:text-7xl">
+                            Sorry!
+                        </h1>
+                    </div>
+                    <div className="w-full py-5">
+                        <p className="font-medium text-lg sm:text-lg md:text-lg lg:text-xl">
+                            {error}
+                        </p>
+                    </div>
+                    <a
+                        href="/"
+                        className="py-2 border-b-2 font-medium border-cyan-700 hover:scale-110 duration-200 text-sm sm:text-base md:text-base lg:text-base"
+                    >
+                        Back to homepage
+                    </a>
+                </div>
+            </div>
+        );
+    }
 
-  return (
-    <div className='max-w-7xl mx-auto mt-4'>
-    {jobs && (
-      <div className='flex flex-col mb-4'>
-        <h2 className='font-semibold text-2xl'>{jobs.title}</h2>
-        <span className='pt-3 text-[18px] font-medium text-gray-400'>
-          Expire Date: 
-          {new Date(
-              jobs.dateline
-          ).toLocaleDateString()}
-        </span>
-        <div className='flex flex-col'>
-          <h2 className='font-semibold text-2xl pt-3'>Job Location:</h2>
-          <ul className='pl-5 pt-3 list-disc'>
-            <li className='font-semibold text-[18px]'>
-              {jobs.location}
-            </li>
-          </ul>
-        </div>
-        <div className='mt-6'>
-          <h2 className='font-semibold text-2xl text-sky-500'>Duties & Responsibilities</h2>
-          <ul className='pl-5 pt-3 list-disc font-medium text-[18px] '>
-              {jobs.responsible
-                ? jobs.responsible
-                .split('.')
-                .filter(responsibility => responsibility.trim() !== '')
-                .map((responsibility, index) => (
-                    <li key={index}>{responsibility.trim()}</li>
-                  ))
-                : <li>No responsibilities listed.</li>
-              }
-          </ul>
-        </div>
-        <div className='mt-6'>
-          <h2 className='font-semibold text-2xl text-sky-500'>Key Selection Criteria and Qualification</h2>
-          <ul className='pl-5 pt-3 list-disc font-medium text-[18px] '>
-              {jobs.requirement
-                ? jobs.requirement
-                .split('.')
-                .filter(requirement => requirement.trim() !== '')
-                .map((requirements, index) => (
-                    <li key={index}>{requirements.trim()}</li>
-                  ))
-                : <li>No responsibilities listed.</li>
-              }
-          </ul>
-        </div>
-        <div className='mt-6'>
-          <h2 className='font-semibold text-2xl text-sky-500'>Benefit Packages:</h2>
-          <ul className='pl-5 pt-3 list-disc font-medium text-[18px] '>
-            {jobs.benefit
-                ? jobs.benefit
-                .split('.')
-                .filter(benefit => benefit.trim() !== '')
-                .map((benefits, index) => (
-                    <li key={index}>{benefits}</li>
-                  ))
-                : <li>No responsibilities listed.</li>
-              }
-          </ul>
-        </div>
-        <div className='mt-6'>
-          <h2 className='font-semibold text-2xl text-sky-500'>How to Apply</h2>
-          <span>Apply:info@chhatgroup.com</span>
-          <span>Phone: 012 890 801</span>
-          <p>
-            Head Office Address:Borey Piphup Thmey La Sen Sok 2 #47-49, Street BT-08M, Phum Krang Angkrong, Sangkat Krang Thnong, Khan Sen Sok, 
-          Phnom Penh, Cambodia, Phnom Penh, Cambodia, 120804
-          </p>
-        </div>
-        <div className='mt-6'>
-          <button className='w-[100px] p-2 bg-sky-400 text-white 
-                            hover:bg-sky-500 font-semibold rounded-full'>
-              Apply Now
-          </button>
-        </div>
-      </div>
-    )}
-      
-    </div>
-  )
-}
+    if (!jobs) {
+        return <p>No job details found.</p>;
+    }
 
-export default CareerDetails
+    return (
+        <div className="max-w-7xl mx-auto mb-5 px-5">
+            <div className="bg-cyan-100 py-4 px-5 rounded-md text-gray-600">
+                <h2 className="text-xl">{jobs.title}</h2>
+                <div className="flex justify-between items-center mb-2">
+                    <span className="pt-3 font-base">
+                        Expire Date: &nbsp;
+                        {new Date(jobs.dateline).toLocaleDateString()}
+                    </span>
+                    <div className="flex items-center text-base">
+                        <h2>Location:</h2>
+                        <p>&nbsp;{jobs.location}</p>
+                    </div>
+                </div>
+            </div>
+            <div className="mt-6 px-5">
+                <h2 className="text-lg text-cyan-700">
+                    Duties & Responsibilities
+                </h2>
+                <ul className="pl-5 pt-3 list-disc text-base text-gray-700">
+                    {jobs.responsible ? (
+                        jobs.responsible
+                            .split(".")
+                            .filter(Boolean)
+                            .map((responsibility, index) => (
+                                <li key={index}>{responsibility.trim()}</li>
+                            ))
+                    ) : (
+                        <li>No responsibilities listed.</li>
+                    )}
+                </ul>
+            </div>
+            <div className="mt-6 px-5">
+                <h2 className="text-lg text-cyan-700">
+                    Key Selection Criteria and Qualification
+                </h2>
+                <ul className="pl-5 pt-3 list-disc text-gray-700">
+                    {jobs.requirement ? (
+                        jobs.requirement
+                            .split(".")
+                            .filter(Boolean)
+                            .map((requirement, index) => (
+                                <li key={index}>{requirement.trim()}</li>
+                            ))
+                    ) : (
+                        <li>No requirements listed.</li>
+                    )}
+                </ul>
+            </div>
+            <div className="mt-6 px-5">
+                <h2 className="text-lg text-cyan-700">Benefit Packages:</h2>
+                <ul className="pl-5 pt-3 list-disc text-gray-700">
+                    {jobs.benefit ? (
+                        jobs.benefit
+                            .split(".")
+                            .filter(Boolean)
+                            .map((benefit, index) => (
+                                <li key={index}>{benefit.trim()}</li>
+                            ))
+                    ) : (
+                        <li>No benefits listed.</li>
+                    )}
+                </ul>
+            </div>
+            <div className="mt-6 px-5 text-gray-700">
+                <h2 className="text-xl text-cyan-700 mb-3">How to Apply</h2>
+                <a
+                    href="mailto:info@chhatgroup.com"
+                    className="hover:text-cyan-700 hover:scale-150 duration-200 underline"
+                >
+                    Email: info@chhatgroup.com
+                </a>
+                <p>Phone: +855 12 890 801 / 10 969 005</p>
+                <p>
+                    Head Office Address: Borey Piphup Thmey La Sen Sok 2 #47-49,
+                    Street BT-08M, Phum Krang Angkrong, Sangkat Krang Thnong,
+                    Khan Sen Sok, Phnom Penh, Cambodia, 120804
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default CareerDetails;
